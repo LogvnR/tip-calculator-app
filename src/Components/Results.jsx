fcba94d26b2b965bf72573d1f2836496c259ae15
LogvnR <logvnjs@gmail.com>
2021-12-31 11:39:44 -0800

    results style changes

--- src/Components/Results.jsx
+++ src/Components/Results.jsx
@@ -3,22 +3,26 @@ import styles from "./Styles/Results.module.css";
 const Results = (props) => {
   return (
     <div className={styles.container}>
-      <div className={styles.result}>
-        <div className={styles.description}>
-          <p className={styles["description-title"]}>Tip Amount</p>
-          <p className={styles["per-person"]}>/ person</p>
+      <div className={styles["result-container"]}>
+        <div className={styles.result}>
+          <div className={styles.description}>
+            <p className={styles["description-title"]}>Tip Amount</p>
+            <p className={styles["per-person"]}>/ person</p>
+          </div>
+          <div className={styles["amount-container"]}>
+            <p className={styles["amount"]}>{`$${props.tipPerPersonAmount}`}</p>
+          </div>
         </div>
-        <div className={styles["amount-container"]}>
-          <p className={styles["amount"]}>{`$${props.tipPerPersonAmount}`}</p>
-        </div>
-      </div>
-      <div className={styles.result}>
-        <div className={styles.description}>
-          <p className={styles["description-title"]}>Total</p>
-          <p className={styles["per-person"]}>/ person</p>
-        </div>
-        <div className={styles["amount-container"]}>
-          <p className={styles["amount"]}>{`$${props.totalPerPersonAmount}`}</p>
+        <div className={styles.result}>
+          <div className={styles.description}>
+            <p className={styles["description-title"]}>Total</p>
+            <p className={styles["per-person"]}>/ person</p>
+          </div>
+          <div className={styles["amount-container"]}>
+            <p
+              className={styles["amount"]}
+            >{`$${props.totalPerPersonAmount}`}</p>
+          </div>
         </div>
       </div>
       <div className={styles["btn-container"]}>
